fbd9445273e5c905bd0055e9379030a4ebf51d8d
ErrorDreemurr <65482653+ErrorDreemurr@users.noreply.github.com>
2022-08-09 16:40:04 +0800

    Delete config.lua

--- config.lua
+++
@@ -1,8 +0,0 @@
-compiler.driver = 'gcc'
-compiler.c_compiler = false
-compiler.cxx_compiler = false
-stackprotector.value = true
-minmacosxsdk.value = false
-macosxppc.value = false
-zlib.value = true
-zlib.include_path = false
